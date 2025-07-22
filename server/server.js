df2b3aa4c8f6e41f2fac35c14db9aa6a6bacb6d3
thienhuyz <phamhuynhthienhuy001@gmail.com>
2025-07-22 23:49:32 +0700

    #2 Add config database

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 require('dotenv').config()
-
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 8888
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.use('/', (req, res) => { res.send('SERVER ON') })
+dbConnect()
+initRoutes(app)
 
 app.listen(port, () => {
     console.log('Server running on the port: ' + port)
