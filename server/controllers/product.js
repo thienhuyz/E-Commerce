3b50697ca5dc511295538c499cb0ad08a5d19d95
thienhuyz <phamhuynhthienhuy001@gmail.com>
2025-07-28 14:21:16 +0700

    #7 Add CRUD Product

---
+++ server/controllers/product.js
@@ -0,0 +1,57 @@
+const Product = require('../models/product')
+const asyncHandler = require('express-async-handler')
+const slugify = require('slugify')
+
+const createProduct = asyncHandler(async (req, res) => {
+    if (Object.keys(req.body).length === 0) throw new Error('Missing inputs')
+    if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
+    const newProduct = await Product.create(req.body)
+    return res.status(200).json({
+        success: newProduct ? true : false,
+        createProduct: newProduct ? newProduct : 'Cannot create new product'
+    })
+})
+
+const getProduct = asyncHandler(async (req, res) => {
+    const { pid } = req.params
+    const product = await Product.findById(pid)
+    return res.status(200).json({
+        success: product ? true : false,
+        productData: product ? product : 'Cannot get product'
+    })
+})
+
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find()
+    return res.status(200).json({
+        success: products ? true : false,
+        productData: products ? products : 'Cannot update products'
+    })
+})
+
+const updateProduct = asyncHandler(async (req, res) => {
+    const { pid } = req.params
+    if (req.body && req.body.title) req.body.slug = slugify(req.body.title)
+    const updatedProduct = await Product.findByIdAndUpdate(pid, req.body, { new: true })
+    return res.status(200).json({
+        success: updatedProduct ? true : false,
+        productData: updatedProduct ? updatedProduct : 'Cannot get products'
+    })
+})
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { pid } = req.params
+    const deleteProduct = await Product.findByIdAndDelete(pid)
+    return res.status(200).json({
+        success: deleteProduct ? true : false,
+        productData: deleteProduct ? deleteProduct : 'Cannot delete products'
+    })
+})
+
+module.exports = {
+    createProduct,
+    getProduct,
+    getProducts,
+    updateProduct,
+    deleteProduct
+}
