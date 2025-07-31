9da35ca3d576fef830a8158a68e3d28df2ddbfe8
thienhuyz <phamhuynhthienhuy001@gmail.com>
2025-08-01 04:45:03 +0700

    #11 Add CRUD Blog Category

---
+++ server/controllers/blogCategory.js
@@ -0,0 +1,43 @@
+const BlogCategory = require('../models/blogCategory');
+const asyncHandler = require('express-async-handler');
+
+const createCategory = asyncHandler(async (req, res) => {
+    const respone = await BlogCategory.create(req.body);
+    return res.json({
+        success: respone ? true : false,
+        createProductCategory: respone ? respone : 'Cannot create new blog-category',
+    })
+})
+
+const getCategories = asyncHandler(async (req, res) => {
+    const respone = await BlogCategory.find().select('title _id')
+    return res.json({
+        success: respone ? true : false,
+        blogCategories: respone ? respone : 'Cannot get blog-category',
+    })
+})
+
+const updateCategory = asyncHandler(async (req, res) => {
+    const { bcid } = req.params
+    const respone = await BlogCategory.findByIdAndUpdate(bcid, req.body, { new: true });
+    return res.json({
+        success: respone ? true : false,
+        updateCategory: respone ? respone : 'Cannot update blog-category',
+    })
+})
+
+const deleteCategory = asyncHandler(async (req, res) => {
+    const { bcid } = req.params
+    const respone = await BlogCategory.findByIdAndDelete(bcid);
+    return res.json({
+        success: respone ? true : false,
+        deletedCategory: respone ? respone : 'Cannot delete blog-category',
+    })
+})
+
+module.exports = {
+    createCategory,
+    getCategories,
+    updateCategory,
+    deleteCategory
+}
