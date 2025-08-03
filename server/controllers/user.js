b38a4e117b61aa407348e7ac009205cff1609a91
thienhuyz <phamhuynhthienhuy001@gmail.com>
2025-08-03 13:32:44 +0700

    #17 Add Cart User

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -182,6 +182,35 @@ const updateUserAddress = asyncHandler(async (req, res) => {
     })
 })
 
+const updateCart = asyncHandler(async (req, res) => {
+    const { _id } = req.user
+    const { pid, quantity, color } = req.body
+    if (!pid || !quantity || !color) throw new Error('Missing inputs')
+    const user = await User.findById(_id).select('cart')
+    const alreadyProduct = user?.cart.find(el => el.product.toString() == pid)
+    if (alreadyProduct) {
+        if (alreadyProduct.color === color) {
+            const response = await User.updateOne({ cart: { $elemMatch: alreadyProduct } }, { $set: { 'cart.$.quantity': quantity } }, { new: true })
+            return res.status(200).json({
+                success: response ? true : false,
+                deleteUser: response ? response : 'Some thing went wrong'
+            })
+        } else {
+            const response = await User.findByIdAndUpdate(_id, { $push: { cart: { product: pid, quantity, color } } }, { new: true })
+            return res.status(200).json({
+                success: response ? true : false,
+                deleteUser: response ? response : 'Some thing went wrong'
+            })
+        }
+    } else {
+        const response = await User.findByIdAndUpdate(_id, { $push: { cart: { product: pid, quantity, color } } }, { new: true })
+        return res.status(200).json({
+            success: response ? true : false,
+            deleteUser: response ? response : 'Some thing went wrong'
+        })
+    }
+
+})
 module.exports = {
     register,
     login,
@@ -194,5 +223,6 @@ module.exports = {
     deleteUser,
     updateUser,
     updateUserByAdmin,
-    updateUserAddress
+    updateUserAddress,
+    updateCart
 }
